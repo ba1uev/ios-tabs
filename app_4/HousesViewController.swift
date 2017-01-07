45bc7b17e3a5806da0ae37827326739d7e047939
ba1uev <p_baluev@setka.io>
2017-01-07 17:46:54 +0300

    Start table cells w/ houses

---
+++ app_4/HousesViewController.swift
@@ -0,0 +1,49 @@
+//
+//  HousesViewController.swift
+//  app_4
+//
+//  Created by pavel baluev on 07.01.17.
+//  Copyright © 2017 pavel baluev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//struct House {
+//    let pictureURL: String
+//    let text: String
+//    let size: Int
+//    
+//    init(dict: NSDictionary) {
+//        pictureURL = dict["picture"] as! String
+//        text = dict["text"] as! String
+//        size = dict["size"] as! Int
+//    }
+//}
+//
+//
+//func LoadHouses() -> [House] {
+//    if let jsonUrl = URL(string: "https://raw.githubusercontent.com/ilyannn/iOS-Swift-Materials/master/Course17A/HomeSearch/data.json")
+//    {
+//        do {
+//            let jsonData = try Data(contentsOf: jsonUrl)
+//            let json = try JSONSerialization.jsonObject(with: jsonData, options: [])
+//            
+//            let array = json as! [NSDictionary]
+//
+//            return array.map { dict in
+//                House(dict: dict)
+//            }
+//        } catch {}
+//    }
+//    return []
+//}
+//
+//class HousesViewController: UITableViewController {
+//    let houses = LoadHouses()
+//    
+////    override func tableView()
+//}
+
+
+
--- app_4/SecondViewController.swift
+++ app_4/SecondViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import CoreData
+
+class Profile: NSManagedObject {
+    @NSManaged var pictureData: NSData
+}
 
 class SecondViewController: UIViewController, UIImagePickerControllerDelegate,
     UINavigationControllerDelegate {
 
     @IBOutlet weak var profilePicture: UIImageView!
     @IBOutlet weak var explanationText: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let layer = profilePicture.layer
+        //        layer.cornerRadius = layer.bounds.size.width / 2
+        layer.borderWidth = 3
+        layer.borderColor = UIColor.blue.cgColor
+        
+        // add database (core data)
+//        let container = NSPersistentContainer(name: "Model")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
     @IBAction func addPhoto(_ sender: Any) {
         let vc = UIImagePickerController()
@@ -26,19 +49,6 @@ class SecondViewController: UIViewController, UIImagePickerControllerDelegate,
         vc.delegate = self
         present(vc, animated: true, completion: nil)
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let layer = profilePicture.layer
-//        layer.cornerRadius = layer.bounds.size.width / 2
-        layer.borderWidth = 3
-        layer.borderColor = UIColor.blue.cgColor
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     
     override func viewDidLayoutSubviews() {
@@ -49,13 +59,18 @@ class SecondViewController: UIViewController, UIImagePickerControllerDelegate,
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        
+        dismiss(animated: true, completion: {})
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
             profilePicture.image = image
             explanationText.isHidden = true
+            
+//            let context = container.viewContext
+//            let descr = container.manageObjects
+//            let profile = Profile(entity: descr, insertPhoto: context)
+//            profile.pictureData =
         }
         dismiss(animated: true, completion: {})
         
